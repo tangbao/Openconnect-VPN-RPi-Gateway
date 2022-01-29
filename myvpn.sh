b1b1d484e8cfbea9634fb31cca693b2e2c030a3e
tangbao <i@tbis.me>
2022-01-29 08:06:54 +0000

    add blank lines

--- myvpn.sh
+++ myvpn.sh
@@ -4,3 +4,4 @@
 # modify it before using
 
 challange=$(python3 duo.py) && sudo -S <<< "<root password>" echo I am root now && { printf '<NetID password>\n'; sleep 1; printf "$challange\n"; } | sudo openconnect <VPN URL> --user=<NetID>
+
